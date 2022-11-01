d757b6dd10ba52c4614191faf1bf8186cf434a27
Saqib <iamsaqib24@gmail.com>
2022-11-01 22:06:23 +0530

    wiring done

---
+++ areaoftriangle.js
@@ -0,0 +1,12 @@
+const submitButton = document.querySelector('#submit-btn');
+const outputArea = document.querySelector('#output');
+const baseInput = document.querySelector('#base-input');
+const heightInput = document.querySelector('#height-input')
+
+function calculateArea() {
+    // console.log("clicked");
+    // console.log(baseInput.value);
+    // console.log(heightInput.value);
+}
+
+submitButton.addEventListener('click', calculateArea);
